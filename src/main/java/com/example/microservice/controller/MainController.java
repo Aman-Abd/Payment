3602d40da5069957694ce0e669a9671d4520f880
daminnaa-d <dilaratemirtay@gmail.com>
2021-10-24 22:46:45 +0600

    Fixed swagger2

--- src/main/java/com/example/microservice/controller/MainController.java
+++ src/main/java/com/example/microservice/controller/MainController.java
@@ -1,16 +1,19 @@
 package com.example.microservice.controller;
 
-import com.example.microservice.database.DbService;
 import com.example.microservice.entities.Order;
 import com.example.microservice.entities.Payment;
 import com.example.microservice.repository.PaymentRepository;
 import com.example.microservice.services.MainService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("payment")
+@Api(value = "Payment Controller class", description = "This class allows to interact with Payment object")
 public class MainController {
     @Autowired
     private PaymentRepository paymentRepository;
@@ -19,11 +22,13 @@ public class MainController {
     MainService mainService;
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Method to get payment info", response = List.class)
     public Order getPaymentInfo (@PathVariable String id){
         return mainService.GetOrderInfo(Integer.parseInt(id));
     }
 
     @PostMapping("{id}")
+    @ApiOperation(value = "Method to pay for order", response = List.class)
     public boolean pay(@PathVariable String id, @RequestBody String amount){
         try{
             Order order = mainService.GetOrderInfo(Integer.parseInt(id));
