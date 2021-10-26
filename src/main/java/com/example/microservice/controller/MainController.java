9e6f0153b73429a3d6cf0ad83b53437ae5d56f73
Aman-Abd <aman.abdyraxmanov@mail.ru>
2021-10-26 20:55:25 +0600

    Added Hystrix

--- src/main/java/com/example/microservice/controller/MainController.java
+++ src/main/java/com/example/microservice/controller/MainController.java
@@ -4,6 +4,8 @@ import com.example.microservice.entities.Order;
 import com.example.microservice.entities.Payment;
 import com.example.microservice.repository.PaymentRepository;
 import com.example.microservice.services.MainService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +25,35 @@ public class MainController {
 
     @GetMapping("{id}")
     @ApiOperation(value = "Method to get payment info", response = List.class)
+    @HystrixCommand(
+            fallbackMethod = "getPaymentInfoFallBack",
+            threadPoolKey = "getPaymentInfo",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="100"),
+                    @HystrixProperty(name="maxQueueSize", value="50"),
+            })
     public Order getPaymentInfo (@PathVariable String id){
         return mainService.GetOrderInfo(Integer.parseInt(id));
     }
 
+    public Order getPaymentInfoFallBack(@PathVariable String id){
+        Order order = new Order();
+        order.setAmount(0);
+        order.setSubscriptionType("Non");
+        order.setUserId(0);
+        order.setValid(false);
+        return order;
+    }
+
     @PostMapping("{id}")
     @ApiOperation(value = "Method to pay for order", response = List.class)
+    @HystrixCommand(
+            fallbackMethod = "payFallBack",
+            threadPoolKey = "pay",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="100"),
+                    @HystrixProperty(name="maxQueueSize", value="50"),
+            })
     public boolean pay(@PathVariable String id, @RequestBody String amount){
         try{
             Order order = mainService.GetOrderInfo(Integer.parseInt(id));
@@ -45,4 +70,9 @@ public class MainController {
             return false;
         }
     }
+
+    public boolean payFallBack(@PathVariable String id, @RequestBody String amount){
+        return false;
+    }
+
 }
